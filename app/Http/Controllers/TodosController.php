19341095672310f6db05eb8276c1ed29dd7cdd75
Anmol Shrestha <anamolshres55@gmail.com>
2020-02-07 00:11:16 +0545

    Added Deleted Function for Todos

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -111,5 +111,11 @@ class TodosController extends Controller
     public function destroy($id)
     {
         //
+
+        $todo = Todo::find($id);
+
+        $todo->delete();
+
+        return redirect('/')->with('success', 'Todo Deleted!');
     }
 }
